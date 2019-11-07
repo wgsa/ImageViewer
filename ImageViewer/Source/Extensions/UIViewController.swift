0ccfd3664a5de912aba3645c70f990769e1a3645
Jonny Johansson <jonny.johansson@schibsted.com>
2019-11-07 16:43:42 +0100

    Upgraded to Swift 5

--- ImageViewer/Source/Extensions/UIViewController.swift
+++ ImageViewer/Source/Extensions/UIViewController.swift
@@ -10,12 +10,12 @@ import UIKit
 
 public extension UIViewController {
     
-    public func presentImageViewer(_ imageViewer: ImageViewerController, completion: (() -> Void)? = {}) {
+    func presentImageViewer(_ imageViewer: ImageViewerController, completion: (() -> Void)? = {}) {
         
         present(imageViewer, animated: true, completion: completion)
     }
     
-    public func presentImageGallery(_ gallery: GalleryViewController, completion: (() -> Void)? = {}) {
+    func presentImageGallery(_ gallery: GalleryViewController, completion: (() -> Void)? = {}) {
         
         present(gallery, animated: true, completion: completion)
     }
